f61c87c5349e2f795de05e803efe20e3f8e7d62a
Francisco Rangel <francorangelcoronado@gmail.com>
2023-12-04 21:04:12 -0600

    empezando con los pokemons names

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="options-container">
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'PokemonOptions'
+}
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+}
+li {
+    background-color: white;
+    border-radius: 8px;
+    border: 1px solid rgba(255, 0, 0, 0.2);
+    cursor: pointer;
+    margin-bottom: 15px;
+    width: 250px;
+}
+li:hover {
+    background-color: rgb(251, 247, 0, 0.4);
+}
+.options-container {
+    display: flex;
+    justify-content: center;
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>Who is this Pokemon?</h1>
+<PokemonPicture :pokemonId="5" :showPokemon="false" />
+<PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+console.log(getPokemonOptions())
+
+export default {
+    name: 'PokemonPage',
+    components: {
+        PokemonOptions,
+        PokemonPicture
+   }
+}
+</script>
